eede75077592ee8f874641bee424404823a6038a
PatrizioFiori <patrizio.fiori1995@gmail.com>
2025-05-13 18:09:54 +0200

    Implement type checking and logging for unknown data types

---
+++ README.md
@@ -0,0 +1,14 @@
+# ex-js-snack-typescript
+
+## 🏆 Snack 1
+Hai ricevuto un dato generico da un'API, ma non sai di che tipo sia… Il tuo compito è controllare il tipo del dato e stampare il valore in modo corretto.
+
+Se è una stringa: stampala in maiuscolo
+Se è un numero: moltiplicalo per due e stampalo
+Se è un booleano: stampa “Sì” o “No” in base al suo valore
+In tutti gli altri casi: stampa “Tipo non supportato”
+
+### 🎯 BONUS
+Se è null: stampa “Il dato è vuoto”
+Se è un array: stampa la sua lunghezza
+Se è una Promise: attendi che si risolva e stampa il valore del resolve.
--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,31 @@
-console.log("cia2o")
+let dato: unknown
+dato = []
+
+if (typeof dato == "string") {
+  dato = dato.toUpperCase()
+  console.log(dato, "è una stringa");
+
+} else if (typeof dato == "number") {
+  dato = dato * 2
+  console.log(dato, "è un number");
+
+} else if (typeof dato == "boolean") {
+  console.log(dato, "è un booleano");
+
+} else if (typeof dato == "object") {
+  if (dato == null) {
+    console.log("dato ha valore", dato);
+
+  } else if (Array.isArray(dato)) {
+
+    console.log("dato è un array", dato);
+
+  } else if (dato instanceof Promise) {
+    dato.then((res) => {
+      console.log(res, "il valore risolto della promise");
+
+    })
+  } else {
+    console.log("oggetto generico")
+  }
+}
